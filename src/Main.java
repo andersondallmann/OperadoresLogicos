896bbdfcd6df3ac8b792df2b4ba9384ef936a301
andersondallmann <andersondallmann@hotmail.com>
2022-12-23 09:20:40 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Operadores relacionais
+        //> < <= >= != ==
+        int num1, num2;
+        num1 = 30;
+        num2 = 20;
+
+        System.out.println("------------------------------------------------------------");
+        if (num1 == num2) {
+            System.out.println("O números são iguais");
+        } else if ( num1 > num2) {
+            System.out.println("O número 1 é maior que o numero2");
+        } else {
+            System.out.println("O número 2 é maior que o numero1");
+        }
+        System.out.println("------------------------------------------------------------");
+    }
+}
